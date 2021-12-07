a91ce522dfd40e8f9a0fef5704c87d4e64cb9334
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-12-07 16:37:24 +0530

    Add kth smallest program

---
+++ 1.Array/DS_3_Kth_Smallest_Element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int findKthSmallest(int arr[], int size, int k) {
+    priority_queue<int, vector<int>, greater<int> > p_queue;
+    for(int i = 0; i < size; i++) {
+        p_queue.push(arr[i]);
+    }
+    int ans=0, i=0;
+    while(!p_queue.empty()) {
+        if(i==k) {
+            ans = p_queue.top();
+            break;
+        }
+        i++;
+        p_queue.pop();
+    }
+    return ans;
+}
+
+
+int main() {
+    int k=2;
+    int arr[] = { 7, 1, 5, 2, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int kthSmall = findKthSmallest(arr, size, k);
+    cout << kthSmall << endl;
+}
